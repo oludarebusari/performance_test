772812350c64faaa24e8a6203dde2861eea8ec38
Oludare Busari <oludare.busari@gmail.com>
2025-05-06 18:50:25 -0600

    modify todo file

--- environmentConfig.js
+++ environmentConfig.js
@@ -1,16 +1,16 @@
 // snippet from ./environmentConfig.js
 export function developmentConfig(filename) {
     let envConfig = {
-      // BASE_URL: "http://todo-app-barkend.herokuapp.com/todos/",
-      BASE_URL: "http://www.saucedemo.com/",
+      BASE_URL: "http://todo-app-barkend.herokuapp.com/todos/",
+      // BASE_URL: "http://www.saucedemo.com/",
       ENV: "DEVELOPMENT",
     };
     return Object.assign({}, envConfig, filename);
   }
    export function stagingConfig(filename) {
     let envConfig = {
-      // BASE_URL: "https://todo-app-barkend.herokuapp.com/todos/",
-      BASE_URL: "https://www.saucedemo.com/",
+      BASE_URL: "https://todo-app-barkend.herokuapp.com/todos/",
+      // BASE_URL: "https://www.saucedemo.com/",
       ENV: "STAGING",
     };
     return Object.assign({}, envConfig, filename);
--- todos-testing.js
+++ todos-testing.js
@@ -1,38 +1,44 @@
 // snippet from ./todos-testing.js
-i
+import http from 'k6/http';
+import { group, check } from 'k6';
 
 export let options = {
-    scenarios: {
-      example_scenario: {
-        env: { EXAMPLEVAR: 'testing' },
-        tags: { example_tag: 'testing' },
-      },
-    }
-  }
+  scenarios: {
+    example_scenario: {
+      executor: 'shared-iterations',
+      vus: 1,
+      iterations: 1,
+      env: { EXAMPLEVAR: 'testing' },
+      tags: { example_tag: 'testing' },
+    },
+  },
+};
 
-group('Create a Todo', () => {
-    const response = http.post('https://todo-app-barkend.herokuapp.com/todos/',
-    {"task": "write k6 tests"}
+export default function () {
+  let todoID;
+
+  group('Create a Todo', () => {
+    const response = http.post(
+      'https://todo-app-barkend.herokuapp.com/todos/',
+      JSON.stringify({ task: 'write k6 tests' }),
+      { headers: { 'Content-Type': 'application/json' } }
     );
-    todoID = response.json()._id;
-    check(response, {
-        "status code should be 200": res => res.status === 200,
-    });
-    check(response, {
-        "Response should have created todo": res => res.json().completed === false,
-    });
-})
 
-group('get a todo item', () => {
-    const response = http.get(`https://todo-app-barkend.herokuapp.com/todos/${todoID}`
-       );
+    todoID = response.json()._id;
+
     check(response, {
-        "status code should be 200": res => res.status === 200,
+      'status code should be 200': res => res.status === 200,
+      'Response should have created todo': res => res.json().completed === false,
     });
+  });
+
+  group('Get a Todo item', () => {
+    const response = http.get(`https://todo-app-barkend.herokuapp.com/todos/${todoID}`);
+
     check(response, {
-        "response should have the created todo": res => res.json()[0]._id === todoID,
+      'status code should be 200': res => res.status === 200,
+      'response should have the created todo': res => res.json()[0]._id === todoID,
+      'response should have the correct state': res => res.json()[0].completed === false,
     });
-    check(response, {
-        "response should have the correct state": res => res.json()[0].completed === false,
-    });
-   })
+  });
+}
